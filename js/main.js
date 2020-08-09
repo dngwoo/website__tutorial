97bd90413a85e882ff089c78d4aeee404202c327
강현준 <kanghj@ganghyeonjun-ui-MacBookPro.local>
2020-08-10 00:48:29 +0900

    MainPage Darkmode

--- js/main.js
+++ js/main.js
@@ -3,18 +3,74 @@ window.addEventListener('load', function () {
 
   let width = (description.clientWidth + 1);
   let i = 0;
-  step = 3;
+  step = 1;
   function go() {
-    // console.log(description.clientWidth);
-    i = i < width ? i + step : -15;
-    console.log(i);
+    i = i < width ? i + step : -42;
+    // console.log(i);
     description.style.marginLeft = -i + 'px';
   }
-  // space = '&nbsp';
   let text = description.innerHTML;
   description.innerHTML = text + text + text + text;
-  // description.innerHTML = space + text + space;
-  // description.style.position = 'absolute';
-  // description.style.position = '';
   setInterval(go, 10);
 }, true);
+
+(function () {
+  const darkmodeBtn = document.querySelector('.darkmode__btn');
+  const darkmodeRound = document.querySelector('.darkmode__round');
+  const html = document.getElementsByTagName('html')[0];
+  let mode = "light";
+
+  const osTheme = () => {
+    let osMode = "light";
+    if (matchMedia) {
+      osMode = matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light";
+      html.classList[osMode === "dark" ? "add" : "remove"]("darkmode");
+    }
+  }
+
+  const handleDarkmode = () => {
+    if (darkmodeRound.className.includes('left')) {
+      mode = "osMode"
+      darkmodeRound.classList.remove('left');
+      darkmodeRound.classList.add('center--r');
+      osTheme();
+    }
+    else if (darkmodeRound.className.includes('center--r')) {
+      mode = "dark"
+      darkmodeRound.classList.remove('center--r');
+      darkmodeRound.classList.add('right');
+      if (!html.className.includes("darkmode")) {
+        html.classList.add('darkmode');
+      }
+    }
+    else if (darkmodeRound.className.includes('right')) {
+      mode = "osTheme"
+      darkmodeRound.classList.remove('right');
+      darkmodeRound.classList.add('center--l');
+      osTheme();
+    }
+    else if (darkmodeRound.className.includes('center--l')) {
+      mode = "light"
+      darkmodeRound.classList.remove('center--l');
+      darkmodeRound.classList.add('left');
+      if (html.className.includes("darkmode")) {
+        html.classList.remove('darkmode');
+      }
+    }
+    else {
+      return;
+    }
+  }
+
+  darkmodeBtn.addEventListener('click', handleDarkmode);
+
+  const init = () => {
+    if (matchMedia) {
+      ['light', 'dark'].forEach(osMode => {
+        matchMedia(`(prefers-color-scheme: ${osMode})`).addListener(e => { if (e.matches && mode === "osMode") { osTheme() } })
+      })
+    }
+  }
+  init();
+})();
+
